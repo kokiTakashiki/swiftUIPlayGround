9d721e865d619323bb28a72a4873bb6f30ea81ec
koki T <bluewhitered123@gmail.com>
2021-05-15 15:54:51 +0900

    はじまり

---
+++ PageViewAndCollectionViewTabUI/PageViewAndCollectionViewTabUI/PageViewAndCollectionViewTabUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageViewAndCollectionViewTabUIApp.swift
+//  PageViewAndCollectionViewTabUI
+//
+//  Created by 武田孝騎 on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct PageViewAndCollectionViewTabUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
