8da79d192eef5974f3c372c45ee8e29c770d6ba5
koki T <bluewhitered123@gmail.com>
2021-05-16 18:19:02 +0900

    tableViewTest

---
+++ tableViewUI/tableViewUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tableViewUI
+//
+//  Created by 武田孝騎 on 2021/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tableViewUI/tableViewUI/tableViewUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  tableViewUIApp.swift
+//  tableViewUI
+//
+//  Created by 武田孝騎 on 2021/05/16.
+//
+
+import SwiftUI
+
+@main
+struct tableViewUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
