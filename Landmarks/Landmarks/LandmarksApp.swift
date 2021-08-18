1ee00481a10eda64b774f47b00b802d885c43094
koki T <bluewhitered123@gmail.com>
2021-08-18 17:59:42 +0900

    SwiftUI tutorial進めてます。Building Lists and NavigationのSection 2 Create the Row Viewに入ったところ。

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 武田孝騎 on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,8 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by 武田孝騎 on 2021/08/18.
+//
+
+import Foundation
