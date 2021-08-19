1829f89e5985b26ac7795ca37659c4fc1e3d785d
koki T <bluewhitered123@gmail.com>
2021-08-19 20:57:17 +0900

    Section 2　Create the Row Viewまで進めた。

--- Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -2,7 +2,29 @@
 //  LandmarkRow.swift
 //  Landmarks
 //
-//  Created by 武田孝騎 on 2021/08/18.
+//  Created by 武田孝騎 on 2021/08/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[1])
+            .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
