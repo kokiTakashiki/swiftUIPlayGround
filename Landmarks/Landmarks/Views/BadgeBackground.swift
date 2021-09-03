80ba72307b2f9d87c4a499c6ea41718f155d14ce
koki T <bluewhitered123@gmail.com>
2021-09-03 11:27:33 +0900

    第2節 バッジの背景を描く create BadgeBackground.swift

---
+++ Landmarks/Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,23 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by 武田孝騎 on 2021/08/26.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            
+        }
+        .fill(Color.black)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
