f0500a08c14ae9ca36f7feced5133701dd68afb4
kokiTakeda <bluewhitered123@gmail.com>
2022-09-09 21:00:52 +0900

    start

---
+++ HiddenAnimationTabBar_iOS14Support/HiddenAnimationTabBar_iOS14Support/HiddenAnimationTabBar_iOS14SupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiddenAnimationTabBar_iOS14SupportApp.swift
+//  HiddenAnimationTabBar_iOS14Support
+//
+//  Created by 武田孝騎 on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct HiddenAnimationTabBar_iOS14SupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
