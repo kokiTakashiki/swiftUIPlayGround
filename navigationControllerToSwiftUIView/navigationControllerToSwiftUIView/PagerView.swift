86ba1de63f6ed0de99164044f9b2dc4c16de1a06
kokiTakeda <bluewhitered123@gmail.com>
2022-09-30 18:38:37 +0900

    スワイプ判定を削除。

--- navigationControllerToSwiftUIView/navigationControllerToSwiftUIView/PagerView.swift
+++ navigationControllerToSwiftUIView/navigationControllerToSwiftUIView/PagerView.swift
@@ -12,9 +12,9 @@ where
 Content: View {
     
     @Binding private var index: Int
-    @Binding private var swipeable: Bool
+
     @State private var offset: CGFloat = 0
-    @State private var show: Bool = true
+
     private let content: Content
     private let endIndex: Int
     
@@ -25,12 +25,10 @@ Content: View {
     
     init(index: Binding<Int>,
          endIndex: Int,
-         swipeable: Binding<Bool>,
          @ViewBuilder content: @escaping () -> Content
     ) {
         self.content = content()
         self._index = index
-        self._swipeable = swipeable
         self.endIndex = endIndex
     }
     
@@ -45,31 +43,24 @@ Content: View {
             .content.offset(x: self.offset)
             .frame(width: geometry.size.width, height: nil, alignment: .leading)
             .onChange(of: index) { value in
-                self.show = false
                 withAnimation {
                     self.offset = -geometry.size.width * CGFloat(self.index)
-                    self.show = true
                 }
             }
-            .blur(radius: show ? 0 : 2)
             .gesture(DragGesture()
                 .onChanged({ value in
-                    if swipeable {
-                        self.offset = value.translation.width - geometry.size.width * CGFloat(self.index)
-                    }
+                    self.offset = value.translation.width - geometry.size.width * CGFloat(self.index)
                 })
                 .onEnded({ value in
-                    if swipeable {
-                        let scrollThreshold = geometry.size.width / 2
-                        if value.predictedEndTranslation.width < -scrollThreshold {
-                            self.index = min(self.index + 1, endIndex - 1)
-                        } else if value.predictedEndTranslation.width > scrollThreshold {
-                            self.index = max(self.index - 1, 0)
-                        }
+                    let scrollThreshold = geometry.size.width / 2
+                    if value.predictedEndTranslation.width < -scrollThreshold {
+                        self.index = min(self.index + 1, endIndex - 1)
+                    } else if value.predictedEndTranslation.width > scrollThreshold {
+                        self.index = max(self.index - 1, 0)
+                    }
 
-                        withAnimation {
-                            self.offset = -geometry.size.width * CGFloat(self.index)
-                        }
+                    withAnimation {
+                        self.offset = -geometry.size.width * CGFloat(self.index)
                     }
                 })
             )
--- navigationControllerToSwiftUIView/navigationControllerToSwiftUIView/SegmentedPickerExample.swift
+++ navigationControllerToSwiftUIView/navigationControllerToSwiftUIView/SegmentedPickerExample.swift
@@ -25,7 +25,7 @@ struct SegmentedPickerExample: View {
                             },
                             set: {
                                 //model.state.selectionIndex = PageIndex(rawValue: $0) ?? .one
-                                let new = SegmentPickerPageViewModel.MyState(selectionIndex: PageIndex(rawValue: $0) ?? .one, swipeable: false)
+                                let new = SegmentPickerPageViewModel.MyState(selectionIndex: PageIndex(rawValue: $0) ?? .one)
                                 model.changeState(new: new)
                             }),
                         selectionAlignment: .bottom,
