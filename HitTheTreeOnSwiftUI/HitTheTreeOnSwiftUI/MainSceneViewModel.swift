280cbca4031e86806acf92d2ae39bb6b9ed5b588
kokiTakeda <bluewhitered123@gmail.com>
2022-10-17 19:24:50 +0900

    アセットを追加しました。

---
+++ HitTheTreeOnSwiftUI/HitTheTreeOnSwiftUI/MainSceneViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainSceneViewModel.swift
+//  HitTheTreeOnSwiftUI
+//
+//  Created by 武田孝騎 on 2022/10/17.
+//
+
+import SwiftUI
+
+final class MainSceneViewModel: ObservableObject {
+    @MainActor
+    func onAppear() {
+        
+    }
+}
